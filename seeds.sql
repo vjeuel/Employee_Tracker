6cb536b339a9591caba6f37cee9a60682da7391c
vjeuel <vjeuel@gmail.com>
2020-06-20 13:42:01 -0500

    seeds.sql base

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES ();
+
+INSERT INTO role (name, salary, department_id)
+VALUES ();
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ();
+
